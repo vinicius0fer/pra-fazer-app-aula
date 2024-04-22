d0b03bca87be1f61fc88c4ede0de5ecf9f027eac
Edemilton Junior <edemiltonjr@gmail.com>
2024-04-22 20:15:06 -0300

    Criação de telas About, Tasks e Account

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from './src/screens/Login'
 import CreateUser from './src/screens/CreateUser'
+import Tasks from './src/screens/Tasks'
+import Account from './src/screens/Account'
+import About from './src/screens/About'
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator
+            // Abri a página inicial
+            initialRouteName="Tabs"
+            // Aplica estilo na barra de navegação
+            screenOptions={{
+                headerTitleAlign: 'center',
+                headerTintColor: '#FFF',
+                headerStyle: { backgroundColor: '#F60' }
+            }}
+            >
 
                 <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
 
-                <Stack.Screen name="CreateUser" component={CreateUser} />
+                <Stack.Screen name="CreateUser" component={CreateUser} options={{ title: 'Cadastro de Usuário'}} />
 
+                <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
+    );
+}
+
+// Cria o menu do rodapé do aplicativo
+function Tabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Tasks" component={Tasks} options={{ title: 'Tarefas'}} />
+            <Tab.Screen name="Account" component={Account} options={{ title: 'Dados do usuário'}} />
+            <Tab.Screen name="About" component={About} options={{ title: 'Sobre o aplicativo'}} />
+        </Tab.Navigator>
     );
 }
---
+++ src/screens/Account/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Account() {
+  return (
+    <View>
+      <Text>Dados do usuários</Text>
+    </View>
+  )
+}
